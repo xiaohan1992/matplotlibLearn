315f762396ae4a1d39bd02d9d688e839469fca9c
Han.X <xiaohan1992a@163.com>
2018-05-15 11:38:23 +0800

    tick能见度

---
+++ tick能见度.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.linspace(-3,3,50)
+y1=10*x**2+x*3+2
+y2=3*x**3-x**2+6*x-8
+plt.figure(num=0 ,figsize=(8 ,6))
+plt.plot(x,y1,color='green',linewidth=3.0,label='y1')
+plt.plot(x,y2,color='yellow',linewidth=2.0,label='y2')
+plt.legend(loc='lower right')
+#改坐标轴上的值
+new_ticks=np.arange(-2,3)
+plt.xticks(new_ticks)
+plt.yticks([-100,-50,0,50,100],[r'$very\ bad$',r'$bad$',r'$normal$',r'$good$',r'$very\ good$'])
+
+#改坐标轴位置
+ax=plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+ax.spines['bottom'].set_position(('data',0))
+ax.spines['left'].set_position(('data',0))
+
+# tick就是轴上的标值
+for labels in ax.get_xticklabels()+ax.get_yticklabels():
+    labels.set_fontsize(12)
+    labels.set_bbox(dict(facecolor='pink',edgecolor='none',alpha=0.3))
+
+
+plt.show()
